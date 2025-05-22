b6da8559b040bb5218e3a31c61867c8bcb9da48d
ileniaperrella <ilenia.perrella@pal-robotics.com>
2025-05-22 12:58:36 +0200

    add motions for teleop arms (usable only with skip_planning)

--- tiago_pro_bringup/launch/tiago_pro_play_motion2.launch.py
+++ tiago_pro_bringup/launch/tiago_pro_play_motion2.launch.py
@@ -38,6 +38,7 @@ class LaunchArguments(LaunchArgumentsBase):
     end_effector_left: DeclareLaunchArgument = TiagoProArgs.end_effector_left
     wrist_model_right: DeclareLaunchArgument = TiagoProArgs.wrist_model_right
     wrist_model_left: DeclareLaunchArgument = TiagoProArgs.wrist_model_left
+    has_teleop_arms: DeclareLaunchArgument = TiagoProArgs.has_teleop_arms
     use_sim_time:  DeclareLaunchArgument = CommonArgs.use_sim_time
 
 
@@ -68,6 +69,7 @@ def create_play_motion_filename(context):
     ee_left = read_launch_argument('end_effector_left', context)
     wrist_model_right = read_launch_argument('wrist_model_right', context)
     wrist_model_left = read_launch_argument('wrist_model_left', context)
+    has_teleop_arms = read_launch_argument('has_teleop_arms', context)
 
     hw_suffix = get_tiago_pro_hw_suffix(
         arm_right=arm_right,
@@ -98,6 +100,8 @@ def create_play_motion_filename(context):
         ee_motions.append(f"tiago_pro_motions_{ee_left}_left.yaml")
     if ee_right != 'no-end-effector' and arm_right != 'no-arm':
         ee_motions.append(f"tiago_pro_motions_{ee_right}_right.yaml")
+    if has_teleop_arms == 'True':
+        ee_motions.append('tiago_pro_motions_teleop_arms.yaml')
     head_pkg = get_package_share_directory('tiago_pro_head_bringup')
 
     head_motions = [os.path.join(head_pkg, 'config', 'motions',
@@ -108,8 +112,7 @@ def create_play_motion_filename(context):
 
     motion_yamls = [os.path.join(motions_folder, f) for f in motion_files]
     motion_yamls.extend(head_motions)
-    combined_yaml = merge_param_files(motion_yamls)
-
+    combined_yaml = merge_param_files(motion_yamls)     
     motion_planner_file = f"motion_planner{hw_suffix}.yaml"
     motion_planner_config = PathJoinSubstitution([
         pkg_share_dir,
